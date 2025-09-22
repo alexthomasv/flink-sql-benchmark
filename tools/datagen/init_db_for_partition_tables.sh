7ba055d5b680a9b25b719b9de2b88e7e9c602f00
Alex Thomas <alexthomas@berkeley.edu>
2025-09-22 18:23:35 +0000

    Update to work with TF scripts

--- tools/datagen/init_db_for_partition_tables.sh
+++ tools/datagen/init_db_for_partition_tables.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-source ../common/env.sh
+FLINK_BENCH_HOME=/home/psladmin/repo/flink-sql-benchmark
+source $FLINK_BENCH_HOME/tools/common/env.sh
 
-cd ${INSTALL_PATH}/hive-tpcds-setup
-./tpcds-setup_partitioned.sh $SCALE
+cd $FLINK_BENCH_HOME/hive-tpcds-setup
+
+export HIVE_HOME=/usr/local/hive
+export HIVE_BIN="$HIVE_HOME/bin/beeline -u jdbc:hive2://ip-172-31-3-129:10000/default -n psladmin -p ''"
+DEBUG_SCRIPT=1 ./tpcds-setup_partitioned.sh $SCALE
