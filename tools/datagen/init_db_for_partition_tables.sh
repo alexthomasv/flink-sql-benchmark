efdc7d5eb6f87be24d444bf0d35adc37bdda10c9
Alex Thomas <alexthomas@berkeley.edu>
2025-09-23 07:35:05 +0000

    Update datagen

--- tools/datagen/init_db_for_partition_tables.sh
+++ tools/datagen/init_db_for_partition_tables.sh
@@ -7,5 +7,5 @@ source $FLINK_BENCH_HOME/tools/common/env.sh
 cd $FLINK_BENCH_HOME/hive-tpcds-setup
 
 export HIVE_HOME=/usr/local/hive
-export HIVE_BIN="$HIVE_HOME/bin/beeline -u jdbc:hive2://ip-172-31-3-129:10000/default -n psladmin -p ''"
+export HIVE_BIN="$HIVE_HOME/bin/beeline -u jdbc:hive2://localhost:10000/default -n psladmin -p ''"
 DEBUG_SCRIPT=1 ./tpcds-setup_partitioned.sh $SCALE
